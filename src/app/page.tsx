121dc78de917dd030759b9202509e850c54fa3b7
gogicha007 <gogicha@gmail.com>
2025-03-30 14:34:21 +0400

    refactor: main page code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,17 +9,30 @@ export default function Home() {
   const tH = useTranslations('HomePage');
   const tA = useTranslations('AuthForm');
 
+  const renderWelcomeMessage = () => {
+    if (currentUser) {
+      return `${tH('welcomeBack')} ${currentUser.email}`;
+    }
+    return tH('welcome');
+  };
+
+  const renderAuthLinks = () => {
+    if (!currentUser) {
+      return (
+        <div className={styles.home__auth}>
+          <Link href="/sign-in">{tA('login')}</Link>
+          <Link href="/sign-up">{tA('register')}</Link>
+        </div>
+      );
+    }
+    return null;
+  };
+
   return (
     <div className={styles.home}>
       <div className={styles.home__welcome}>
-        {currentUser && `${tH('welcomeBack')} ${currentUser.email}`}
-        {!currentUser && tH('welcome')}
-        {!currentUser && (
-          <div className={styles.home__auth}>
-            <Link href="/sign-in">{tA('login')}</Link>
-            <Link href="/sign-up">{tA('register')}</Link>
-          </div>
-        )}
+        {renderWelcomeMessage()}
+        {renderAuthLinks()}
       </div>
       <div className={styles.home__menu}>HOME PAGE MENU ITEMS</div>
     </div>
