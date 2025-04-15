ab40699fc304e6d68080933aa3462463d66de28b
gogicha007 <gogicha@gmail.com>
2025-04-15 19:55:39 +0400

    feat: about on main page

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import styles from './about.module.css';
+import Link from 'next/link';
 
 const About = () => {
   return (
-    <details>
-      <summary className=''>About project</summary>
-      <h2>Developers</h2>
+    <details className={styles.about} open>
+      <summary className={styles.about__title}>About the project</summary>
+      <p className={styles.about__header}>Developers</p>
+      <div className={styles.about__authors}>
+        <Link href={'https://github.com/gogicha007'} target="_blank">
+          Irakli Gogicha
+        </Link>
+        <Link href={'https://github.com/sheritsh'} target="_blank">
+          Oleg Polovinko
+        </Link>
+        <Link href={'https://github.com/Barvinko'} target="_blank">
+          Vlad Barvinko
+        </Link>
+      </div>
+      <p className={styles.about__header}>Project</p>
+      <p>REST Client app - A light-weight version of Postman created with Next JS</p>
+      <p className={styles.about__header}>Course</p>
+      <p>RS School React JS</p>
       <ul>
-        <li>Irakli</li>
-        <li>Oleg</li>
-        <li>Vlag</li>
+        <li></li>
+        <li></li>
+        <li></li>
       </ul>
     </details>
   );
