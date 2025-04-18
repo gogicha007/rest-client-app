0e2d2bb37b1aee463904d799fe81ae959170febc
Barvinko <vlad.barvinko@gmail.com>
2025-04-19 01:14:43 +0300

    feat: add tests for error.tsx

---
+++ src/app/history/error.test.tsx
@@ -0,0 +1,33 @@
+import { render, screen } from '@testing-library/react';
+import ErrorPage from './error';
+import { ErrorFallbackProps } from '@/components/ErrorFallback';
+import '@testing-library/jest-dom';
+
+jest.mock('@/components/ErrorFallback', () => ({
+  __esModule: true,
+  default: ({ error, reset, header }: ErrorFallbackProps) => (
+    <div data-testid="error-fallback">
+      <h1>{header}</h1>
+      <p>{error.message}</p>
+      <button onClick={reset}>Reset</button>
+    </div>
+  ),
+}));
+
+jest.mock('next-intl', () => ({
+  useTranslations: () => (key: string) =>
+    ({
+      pageError: 'Something went wrong!',
+    })[key] || key,
+}));
+
+describe('<ErrorPage />', () => {
+  const mockError = new Error('Login failed');
+
+  it('renders ErrorFallback with translated header and error message', () => {
+    render(<ErrorPage error={mockError} />);
+
+    expect(screen.getByTestId('error-fallback')).toBeInTheDocument();
+    expect(screen.getByText('Something went wrong!')).toBeInTheDocument();
+  });
+});
