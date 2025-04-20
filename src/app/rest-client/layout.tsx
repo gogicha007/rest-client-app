e7455f360d460563e12d29c620e5e3bdbf0aca74
Oleg Polovinko <sheritsh.s21@gmail.com>
2025-04-20 12:32:46 +0300

    fix: lazy loading tests for rest client and increased coverage

--- src/app/rest-client/layout.tsx
+++ src/app/rest-client/layout.tsx
@@ -1,13 +1,14 @@
 'use client';
 
 import React from 'react';
-import { RestClient } from '@/components/rest-client';
 
-const RestClientLayout: React.FC = () => {
+const RestClientLayout: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
   return (
     <div className="container">
       <h1 className="title">REST Client</h1>
-      <RestClient />
+      {children}
     </div>
   );
 };
